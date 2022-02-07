1fc31ff2ad2ac25f6224d697ed8e7f9b4281a7df
CODESFRU <97609617+codesfru@users.noreply.github.com>
2022-02-07 10:42:22 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,10 +1,29 @@
 class Presets(object):
-    WELCOME_MESSAGE = """
-<b>Hello.. </b>{}
-<code>my bot brings latest updates of telegram to you
-maintained by</code> <b>{}</b>
-<code>Keep me unblocked to receive new updates related to study .</code>
-    """
+    WELCOME_MESSAGE = *Hello {} !*
+✪ I'm an anime-theme management bot [✨](https://telegra.ph/file/11b5922a33de9968cedfe.jpg)
+────────────────────────
+× *Uptime:* `{}`
+× `{}` *users, across* `{}` *chats.*
+────────────────────────
+✪ Hit /help to see my available commands.
+
+
+buttons = [
+    [
+        InlineKeyboardButton(text="About Emiko Robot", callback_data="emiko_"),
+    ],
+    [
+        InlineKeyboardButton(text="Get Help", callback_data="help_back"),
+        InlineKeyboardButton(
+            text="Try inline!​​", switch_inline_query_current_chat=""
+        ),
+    ],
+    [
+        InlineKeyboardButton(
+            text="➗ Add Emiko To Your Group ➗", url="t.me/EmiexRobot?startgroup=new"),
+    ],
+]
+
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Please Wait...</b>"
     REPLY_ERROR = "<code>Use this command as a reply to any telegram message with out any spaces.</code>"
