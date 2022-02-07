538fdbe30f02a4269c1544191e0396d87fee0a80
CODESFRU <97609617+codesfru@users.noreply.github.com>
2022-02-07 10:48:35 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,25 +1,10 @@
 class Presets(object):
-    WELCOME_MESSAGE = *Hello 
-I'm an anime-theme management bot [✨](https://telegra.ph/file/11b5922a33de9968cedfe.jpg)
-────────────────────────
- 
-
-buttons = [
-    [
-        InlineKeyboardButton(text="About Emiko Robot", callback_data="emiko_"),
-    ],
-    [
-        InlineKeyboardButton(text="Get Help", callback_data="help_back"),
-        InlineKeyboardButton(
-            text="Try inline!​​", switch_inline_query_current_chat=""
-        ),
-    ],
-    [
-        InlineKeyboardButton(
-            text="➗ Add Emiko To Your Group ➗", url="t.me/EmiexRobot?startgroup=new"),
-    ],
-]
-
+    WELCOME_MESSAGE = 
+<b>Hello.. </b>{}
+<code>my bot brings latest updates of telegram to you
+maintained by</code> <b>{}</b>
+<code>Keep me unblocked to receive new updates related to study .</code>
+    
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Please Wait...</b>"
     REPLY_ERROR = "<code>Use this command as a reply to any telegram message with out any spaces.</code>"
