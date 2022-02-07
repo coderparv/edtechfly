92970a29f7d8d825c823b2124463d9a5087c895c
CODESFRU <97609617+codesfru@users.noreply.github.com>
2022-02-07 10:47:01 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,6 +1,6 @@
 class Presets(object):
     WELCOME_MESSAGE = *Hello 
-✪ I'm an anime-theme management bot [✨](https://telegra.ph/file/11b5922a33de9968cedfe.jpg)
+I'm an anime-theme management bot [✨](https://telegra.ph/file/11b5922a33de9968cedfe.jpg)
 ────────────────────────
  
 
