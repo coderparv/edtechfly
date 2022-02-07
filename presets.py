a1e57ceb799dcbb710a5dc2c2b5e6dcb4a798b79
CODESFRU <97609617+codesfru@users.noreply.github.com>
2022-02-07 10:50:31 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,10 +1,10 @@
 class Presets(object):
-    WELCOME_MESSAGE = 
+    WELCOME_MESSAGE = """
 <b>Hello.. </b>{}
 <code>my bot brings latest updates of telegram to you
 maintained by</code> <b>{}</b>
 <code>Keep me unblocked to receive new updates related to study .</code>
-    
+    """
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Please Wait...</b>"
     REPLY_ERROR = "<code>Use this command as a reply to any telegram message with out any spaces.</code>"
