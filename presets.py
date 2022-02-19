51bc8b1082026c930a9f2823bd23fe45e870f459
its parv <90262471+coderparv@users.noreply.github.com>
2022-02-19 12:40:04 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,9 +1,6 @@
 class Presets(object):
     WELCOME_MESSAGE = """
-<b>Hello.. </b>{}
-<code>my bot brings latest updates of telegram to you
-maintained by</code> <b>{}</b>
-<code>Keep me unblocked to receive new updates related to study .</code>
+Very Easy to use me and utilize for your studies keep me check daily for new posts
     """
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Please Wait...</b>"
