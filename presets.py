1efbda729ca3e83d894a6702cc2fdbc80442613a
its parv <90262471+coderparv@users.noreply.github.com>
2022-02-19 12:57:14 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,6 +1,5 @@
 class Presets(object):
-    WELCOME_MESSAGE = """
-Welcome
+    WELCOME_MESSAGE = """ welcome its very easy to utilize me go to the buttons.
     """
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Please Wait...</b>"
