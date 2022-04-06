9f428ab7a2662d6b5541d77b850ac27ceedc2a06
its parv <90262471+coderparv@users.noreply.github.com>
2022-04-06 13:55:05 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,6 +1,5 @@
 class Presets(object):
-    WELCOME_MESSAGE = """ welcome its very easy to utilize me go to the buttons.
-    """
+    WELCOME_MESSAGE = welome 
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Please Wait...</b>"
     REPLY_ERROR = "<code>Use this command as a reply to any telegram message with out any spaces.</code>"
