4e6e8514fe156bb8607daf35d6107be82af3fca4
its parv <90262471+coderparv@users.noreply.github.com>
2022-04-06 14:07:16 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,5 +1,13 @@
 class Presets(object):
-    WELCOME_MESSAGE = welome 
+    WELCOME_MESSAGE = """ welcome i hope you know well what to do now -
+
+1. Make a channel name it as *yourwish*Network #edtechfly_monetize 
+
+2. Post your all channel links there who have 3k + subscribers 
+
+3. join this group https://t.me/+yCxFDUrkVOYyN2I5 send the link and wait for monetization
+
+    """
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Please Wait...</b>"
     REPLY_ERROR = "<code>Use this command as a reply to any telegram message with out any spaces.</code>"
